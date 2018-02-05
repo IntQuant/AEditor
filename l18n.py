28559d6bd690ea152eb02df0f602206e8c8ff7b6
IQuant <quant3234@gmail.com>
2018-02-05 21:46:39 +0300

    localization!

---
+++ l18n.py
@@ -0,0 +1,58 @@
+import pathlib
+import threading
+import sys
+
+from kivy.logger import Logger
+
+current = {}
+load_thread = None
+
+def get(name):
+	global current
+	join()
+	if name in current:
+		return current[name]
+	else:
+		return name
+
+def join():
+	global load_thread
+	if load_thread:
+		load_thread.join()
+		Logger.info("l18n: Joined loading thread")
+	load_thread = None
+
+def get_lang_list():
+	return list(
+	  map(lambda x:x.name, 
+	  filter(lambda x:x.is_dir()==False, 
+	  pathlib.Path("./lang").iterdir()
+	  )))
+	
+def thread_load(lang="en_US"):
+	#print("Starting loader thread")
+	global load_thread
+	load_thread = threading.Thread(target=load, kwargs={"lang":lang})
+	load_thread.start()
+	Logger.info("l18n: Started loader thread")
+
+def load(lang="en_US"):
+	global current
+	
+	if lang!="en_US":
+		load()
+	
+	with open("./lang/"+lang, 'r') as f:
+		for line in f:
+			try:
+				ind, val = line.split("=")
+				current[ind] = val[:-1]
+			except:
+				Logger.warning("l18n: error while parsing '%s'" % line)
+	Logger.info("l18n: Loaded %s" % lang)
+	
+if __name__ == "__main__":
+	print(get_lang_list())
+	thread_load()
+	load_thread.join()
+	print(current)
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import l18n
+import locale
+
+lc = locale.getdefaultlocale()[0]
+available = l18n.get_lang_list()
+fitting = list(filter(lambda x:lc in x, available))
+if len(fitting) > 0:
+	l18n.thread_load(fitting[0])
+else:
+	l18n.thread_load()
+
 from kivy.app import App
 from kivy.uix.widget import Widget
+import kivy.resources
+
+#kivy.resources.resource_add_path('C:\Windows\Fonts')
+
+from editor_visual import VisualEditor
 
 class MainWidget(Widget):
 	pass
@@ -9,5 +25,6 @@ class AEditorApp(App):
 	def build(self):
 		return MainWidget()
 
-
+l18n.join()
+print(l18n.current)
 AEditorApp().run()
